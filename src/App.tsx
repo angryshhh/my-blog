fe4d26778b81cd01a0dc22619f83e515df13f875
cui <angrynaivecui@gmail.com>
2020-02-26 23:05:47 +0800

    get markdown content from file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [markdownText, setMarkdownText] = useState('');
+
+  useEffect(() => {
+    fetch('/articles/20200123.md')
+    .then(response => response.text())
+    .then(text => setMarkdownText(text))
+    .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
-      a
+      <pre>
+        {markdownText}
+      </pre>
     </div>
   );
 }
