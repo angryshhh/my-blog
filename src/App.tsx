9d21a78c8de0dcf0bb6b7fc1af20508dae4be9ce
cui <angrynaivecui@gmail.com>
2020-02-27 00:07:48 +0800

    usee github api to get file list

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ const App: React.FC = () => {
   const [markdownText, setMarkdownText] = useState('');
 
   useEffect(() => {
+    // use github api to get file list
+    fetch('https://api.github.com/repos/angryshhh/my-blog/contents/public/articles')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+
+    // get a markdown file
     fetch(`${window.location.pathname}articles/20200123.md`)
     .then(response => response.text())
     .then(text => setMarkdownText(text))
